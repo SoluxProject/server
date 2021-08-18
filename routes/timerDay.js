5fb1bb9847852b1e643bffa43dee3a832d2b8a57
snghx <nho0814@sookmyung.ac.kr>
2021-08-18 11:57:37 +0900

    Revert "TimerDay 수정"
    This reverts commit bdfa0409c60ca5f272085b646c37c163ce0ff225.

--- routes/timerDay.js
+++ routes/timerDay.js
@@ -9,37 +9,44 @@ const connection = db.init();
 router.get('/list', verifyToken, ctrl.list);
 router.post('/update', verifyToken, ctrl.update);
 
-schedule.scheduleJob('0 0 0 * * *',  async () => {
+schedule.scheduleJob('0 37 11 * * *',  async () => {
     try {
-        const sqlSelectWeek = "SELECT recordWeek from timerWeek";
-        connection.query(sqlSelectWeek, (err, result1) => {
+        //const timerDayid = req.decoded.id; //현재 로그인한 사용자 뿐만 아니라 전체 사용자의 기록 갱신 (수정)
+        const sqlSelectWeek = "SELECT recordWeek from timerWeek WHERE timerWeekid = ?";
+        connection.query(sqlSelectWeek, timerDayid, (err, result1) => {
             if (err) console.log('select 오류' + err);
             else {
-                const sqlSelectDay = "SELECT * from timerDay";
-                connection.query(sqlSelectDay, (err, result2) => {
+                const sqlSelectDay = "SELECT recordDay from timerDay WHERE timerDayid = ?";
+                connection.query(sqlSelectDay, timerDayid, (err, result2) => {
                     if (err) console.log('day select 오류' + err);
                     else {
-                        console.log('affect:'+result1.length);
-                        for(let i =0; i < result1.length; i ++){
-                            const result3 = parseInt(result1[i].recordWeek) + parseInt(result2[i].recordDay);
+                        const result3 = parseInt(result1[0].recordWeek) + parseInt(result2[0].recordDay);
                         const sqlChangeWeek = "UPDATE timerWeek SET recordWeek = ? WHERE timerWeekid = ?";
-                        connection.query(sqlChangeWeek, [result3,result2[i].timerDayid], (err, fin) => {
-                            if (err) console.log('week change 오류'+err);
+                        connection.query(sqlChangeWeek, [result3, timerDayid], (err, fin) => {
+                            if (err) console.log('week change 오류');
                             else {
                                 const sqlDelete = "UPDATE timerDay SET recordDay = 0 ";
                                 connection.query(sqlDelete, (err, result4) => {
                                     if (err) console.log(err);
-                                    else console.log('timer day 초기화 완료');
+                                    else console.log('timer day 초기화 완료 (login)');
                                 })
                             }
                         })
-                        }
                     }
                 })
             }
         })
     } catch (err) {
         console.log(err);
+        const sqlDelete = "UPDATE timerDay SET recordDay = 2 ";
+        connection.query(sqlDelete, (err, result4) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('timer day 초기화 완료');
+            }
+        })
     }
 })
 
